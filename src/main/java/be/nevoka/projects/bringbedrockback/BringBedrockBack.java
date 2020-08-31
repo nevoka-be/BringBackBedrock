08228fd0b86b688a8afbb0a820c9687a5cda7f9b
JayJay1989BE <jonas@lateur.pro>
2020-08-31 13:26:57 +0200

    Bump version to 0.0.3
    Signed-off-by: JayJay1989BE <jonas@lateur.pro>

--- src/main/java/be/nevoka/projects/bringbedrockback/BringBedrockBack.java
+++ src/main/java/be/nevoka/projects/bringbedrockback/BringBedrockBack.java
@@ -16,7 +16,7 @@ public class BringBedrockBack {
 
     public static final String MOD_ID = "bringbedrockback";
     public static final String MOD_NAME = "BringBedrockBack";
-    public static final String VERSION = "0.0.1";
+    public static final String VERSION = "0.0.3";
     public static Logger logger;
 
     @SidedProxy(serverSide = "be.nevoka.projects.bringbedrockback.proxy.ServerProxy", clientSide = "be.nevoka.projects.bringbedrockback.proxy.ClientProxy")
